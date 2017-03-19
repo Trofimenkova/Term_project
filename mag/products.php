bf00db3d76a1496e42954fbc594cd18828f14db7
Trofimenkova <anna.trof@tut.by>
2017-03-19 19:36:22 +0300

    I have added the navigation through pages.

--- mag/products.php
+++ mag/products.php
@@ -2,7 +2,8 @@
     require_once("database.php");
     require_once("models/products.php");
 
+	$page = $_GET['page'];
     $link = db_connect();
-	$products = products_view($link);
+	$products = products_view($link, ($page-1)*12);
     include("views/products.php");
 ?>
