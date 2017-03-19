ef9a9552103aefac94e96434e878c690c6f34f30
Trofimenkova <anna.trof@tut.by>
2017-03-19 21:22:02 +0300

    I have changed the position of the window with aithorization and registration.

--- mag/js/index.js
+++ mag/js/index.js
@@ -1,10 +1,10 @@
 function openWindow(url) {
     var features, w = 350, h = 450;
-	var top = (document.documentElement.clientHeight - h)/2;
+	//var top = (document.documentElement.clientHeight - h)/4;
 	var	left = (document.documentElement.clientWidth - w)/2; 
-    if(top < 0) top = 0;
+    //if(top < 0) top = 0;
 	if(left < 0) left = 0;
-	features = 'top=' + top + ',left=' +left;
+	features = 'top=' + 100 + ',left=' +left;
 	features += ',height=' + h + ',width=' + w + ', scrollbars=no, menubar=no,toolbar=no, location=no,status=no,resizable=no';
 	window.open(url,this.target,features);
 }
