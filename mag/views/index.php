53c7c6530a4d3ecbc53bde27b07a34b58cccf317
Trofimenkova <anna.trof@tut.by>
2017-03-19 10:44:53 +0300

    I have changed captions after authorization.

--- mag/views/index.php
+++ mag/views/index.php
@@ -1,4 +1,10 @@
-﻿<!DOCTYPE html>
+<?php 
+session_start();
+if(!isset($_SESSION["session_username"])) {
+$_SESSION["session_username"] = 0; }
+?>
+
+<!DOCTYPE html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Рыбин Гуд</title>
@@ -9,7 +15,20 @@
 	<![endif]-->
 	<link rel="shortcut icon" href="images/fish.png" type="image/png">
 	<script src="js/index.js"></script>
-
+<script>window.onload = function() {
+	var username = '<?php echo $_SESSION["session_username"];?>';
+	if (username!=0) {
+		document.getElementById("avt").innerHTML = "Личный кабинет";
+		document.getElementById("avt").href = "#";
+		document.getElementById("avt").setAttribute("target","_self");
+		document.getElementById("avt").onclick = function() { };
+		document.getElementById("reg").innerHTML = "Выйти";
+		document.getElementById("reg").href = "logout.php";
+		document.getElementById("reg").setAttribute("target","_self");
+		document.getElementById("reg").onclick = function() { location.reload(); };
+	}
+}
+</script>
 </head>
 <body>
 	<header id="header">
@@ -17,10 +36,9 @@
 			<a href="index.php" id="logo" title="Рыбин Гуд">Рыбин Гуд</a>
 			<div class="right-links">
 				<ul>
-					<li><a href="cart.html"><span class="ico-products"></span>3 товара, 100 BYN</a></li>
-					<li><a href="login.php" id="avt" target="_blank" onclick="return openWindow(this.href);"><span class="ico-account"></span>Авторизация</a></li>
-					<li><a href="register.php" id="reg" target="_blank" onclick="return openWindow(this.href);"><span class="ico-signout"></span>Регистрация</a></li>
-					<li><a href="logout.php"><span class="ico-signout"></span>Выйти</a></li>
+					<li><span class="ico-products"></span><a href="cart.html">3 товара, 100 BYN</a></li>
+					<li><span class="ico-account"></span><a href="login.php" id="avt" target="_blank" onclick="return openWindow(this.href);">Авторизация</a></li>
+					<li><span class="ico-signout"></span><a href="register.php" id="reg" target="_blank" onclick="return openWindow(this.href);">Регистрация</a></li>
 				</ul>
 			</div>
 		</div>
