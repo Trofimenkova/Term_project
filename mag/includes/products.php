ba2a2b37d2e9cd9ab07a9253ccbecebca4645500
Trofimenkova <anna.trof@tut.by>
2017-05-13 10:38:50 +0300

    I have made the include file for navigation.

--- mag/includes/products.php
+++ mag/includes/products.php
@@ -28,21 +28,7 @@
 
 	<div id="body">
 		<div class="container">
-			<div class="pagination">
-				<ul>
-				<li><a href="products.php?page=<?=($_GET['page']-1)?>&sort=<?=$_POST['sort']?>&min_price=<?=$_POST['min_price']?>&max_price=<?=$_POST['max_price']?>" onclick="return stopPrev(<?=($_GET['page']-1)?>)"><span class="ico-prev"></span></a></li>
-				<?php
-				$str = 1;
-				while ($str <= ceil($amount/12)): 
-				?>
-				<li><a href="products.php?page=<?=$str?>&sort=<?=$_POST['sort']?>&min_price=<?=$_POST['min_price']?>&max_price=<?=$_POST['max_price']?>"><?=$str?></a></li>
-				<?php
-				$str++;
-				endwhile;
-				?>
-				<li><a href="products.php?page=<?=($_GET['page']+1)?>&sort=<?=$_POST['sort']?>&min_price=<?=$_POST['min_price']?>&max_price=<?=$_POST['max_price']?>" onclick="return stopNext(<?=($_GET['page']+1)?>, <?=ceil($amount/12)?>)"><span class="ico-next"></span></a></li>
-				</ul>
-			</div>
+			<?php include("includes/navigation.php"); ?>
 			<div class="products-wrap">
 				<aside id="sidebar">
                     <form action="" method="post">
@@ -91,21 +77,7 @@
 				</div>
 				<!-- / content -->
 			</div>
-			<div class="pagination">
-				<ul>
-				<li><a href="products.php?page=<?=($_GET['page']-1)?>&sort=<?=$_POST['sort']?>&min_price=<?=$_POST['min_price']?>&max_price=<?=$_POST['max_price']?>" onclick="return stopPrev(<?=($_GET['page']-1)?>)"><span class="ico-prev"></span></a></li>
-				<?php
-				$str = 1;
-				while ($str <= ceil($amount/12)): 
-				?>
-				<li><a href="products.php?page=<?=$str?>&sort=<?=$_POST['sort']?>&min_price=<?=$_POST['min_price']?>&max_price=<?=$_POST['max_price']?>"><?=$str?></a></li>
-				<?php
-				$str++;
-				endwhile;
-				?>
-				<li><a href="products.php?page=<?=($_GET['page']+1)?>&sort=<?=$_POST['sort']?>&min_price=<?=$_POST['min_price']?>&max_price=<?=$_POST['max_price']?>" onclick="return stopNext(<?=($_GET['page']+1)?>, <?=ceil($amount/12)?>)"><span class="ico-next"></span></a></li>
-				</ul>
-			</div>
+			<?php include("includes/navigation.php"); ?>
 		</div>
 		<!-- / container -->
 	</div>
